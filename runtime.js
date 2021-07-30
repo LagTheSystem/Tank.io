b8ed16698267ccf75e1195f873b241fe6ae4defc
herobrick360 <53086599+herobrick360@users.noreply.github.com>
2021-07-30 17:09:29 -0500

    Fixed Some Semicolon issues
    stupid javascript stuff

--- runtime.js
+++ runtime.js
@@ -371,11 +371,11 @@ if (typeof Object.getPrototypeOf !== "function")
 		var v1y = this.bry - tly;
 		var v2x = x - tlx;
 		var v2y = y - tly;
-		var dot00 = v0x * v0x + v0y * v0y
-		var dot01 = v0x * v1x + v0y * v1y
-		var dot02 = v0x * v2x + v0y * v2y
-		var dot11 = v1x * v1x + v1y * v1y
-		var dot12 = v1x * v2x + v1y * v2y
+		var dot00 = v0x * v0x + v0y * v0y;
+		var dot01 = v0x * v1x + v0y * v1y;
+		var dot02 = v0x * v2x + v0y * v2y;
+		var dot11 = v1x * v1x + v1y * v1y;
+		var dot12 = v1x * v2x + v1y * v2y;
 		var invDenom = 1.0 / (dot00 * dot11 - dot01 * dot01);
 		var u = (dot11 * dot02 - dot01 * dot12) * invDenom;
 		var v = (dot00 * dot12 - dot01 * dot02) * invDenom;
